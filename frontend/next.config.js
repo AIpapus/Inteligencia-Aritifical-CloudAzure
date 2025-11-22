e01e8972ee5d622ab436e7c0bd359590ce945a6b
Juan Pablo Mendoza Rodríguez <61636294+PabloMZR@users.noreply.github.com>
2025-11-22 16:53:35 -0600

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export', // Genera un sitio estático que Azure puede servir
+  distDir: 'build',  // Cambia .next a build (lo que espera el workflow)
+  images: {
+    unoptimized: true, // Necesario para export estático
+  },
+  // Si usas rutas dinámicas, configúralas aquí
+  // trailingSlash: true, // Opcional: añade / al final de las URLs
+}
 
 module.exports = nextConfig
-
